4ee49fc0db3409b80c3021f4c1f0a2e870cdb561
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-07-10 23:41:29 +0900

    ./bin/rake db:migrate

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,19 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710141939) do
+ActiveRecord::Schema.define(version: 20160710143425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "access_tokens", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_access_tokens_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "email"
+    t.string   "email",            null: false
     t.text     "bio"
     t.string   "avatar"
     t.boolean  "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
