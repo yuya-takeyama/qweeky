9f3389fff305919911715206358411675b951f19
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-07-10 23:21:24 +0900

    ./bin/rails g scaffold users username:string email:string bio:text avatar:string admin:boolean

---
+++ server/db/migrate/20160710141939_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.text :bio
+      t.string :avatar
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
